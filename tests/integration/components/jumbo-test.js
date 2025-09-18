2c9d9e625a1c0c5109916c4ab4a8b5c6900eaca5
Giulio Pietroiusti <giulio.pietroiusti@kantox.com>
2025-09-18 14:24:03 +0200

    add some tests for the Jumbo component
    - modify the previously generate file (when we run `ember generate
      component-test jumbo`) by adding some tests.

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -6,21 +6,11 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | jumbo', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders the content inside a jumbo header with a tomster', async function (assert) {
+    await render(hbs`<Jumbo>Hello World</Jumbo>`);
 
-    await render(hbs`<Jumbo />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Jumbo>
-        template block text
-      </Jumbo>
-    `);
-
-    assert.dom().hasText('template block text');
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('Hello World');
+    assert.dom('.jumbo .tomster').exists();
   });
 });
