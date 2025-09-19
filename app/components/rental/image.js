7b409194bec12533d2da3cc79fd39209b97c1b29
Giulio Pietroiusti <giulio.pietroiusti@kantox.com>
2025-09-19 12:24:38 +0200

    enlarge/shrink image on click
    - conditionally render one of two blocks in
      `app/components/rental/image.hbs`;
    
    - add class for rental/image component;
      - add a `isLarge` @tracked property and a `toggleSize` @action for
        it;
    
    - add more tests in
      `tests/integration/components/rental/image-test.js`

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,3 +1,11 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class RentalImage extends Component {}
+export default class RentalImage extends Component {
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
+  }
+}
