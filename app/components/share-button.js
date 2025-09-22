7bdeca6b0f56db417f45bddb30b534340950e49f
Giulio Pietroiusti <giulio.pietroiusti@kantox.com>
2025-09-22 15:18:40 +0200

    test share button
    - test share button
    
    - notice: we had to inject the router service in the share-button
      component.
    
      We want `new URL(this.router.currentURL, window.location.origin);`
      instead of `return window.location.href;`. Otherwise our this
      assertion would fail:
    
      ```
      assert.strictEqual(
         tweetURL.searchParams.get('url'),
         `${window.location.origin}/rentals/grand-old-mansione`,
      );
      ```
    
      Why would it fail? Because ``...during tests, the router is
      configured to maintain the "logical" URL internally, without
      updating the browser's address bar and history entries.'' (See
      https://guides.emberjs.com/release/tutorial/part-2/service-injection/)
    
      So, during tests, the value of `window.location.href` is not what we
      would normally expect.

--- app/components/share-button.js
+++ app/components/share-button.js
@@ -1,10 +1,15 @@
+import { service } from '@ember/service';
 import Component from '@glimmer/component';
 
 const TWEET_INTENT = 'https://twitter.com/intent/tweet';
 
 export default class ShareButton extends Component {
+  @service router;
+  // Note: `@service router;` is a shorthand for `@service('router') router;`.
+  // We can rename the variable in this way: `@service('router') theNameWeWant;`.
+
   get currentURL() {
-    return window.location.href;
+    return new URL(this.router.currentURL, window.location.origin);
   }
 
   get shareURL() {
