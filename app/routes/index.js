c16b545ea6e30a743ba75020410d0cac039cf00f
Giulio Pietroiusti <giulio.pietroiusti@kantox.com>
2025-09-22 17:15:56 +0200

    load model in route
    - use store in
      - app/routes/index.js;
      - app/routes/rental.js.
    
    - configure url builder;
    
    - add a json suffix handler;
    
    - configure request manager;
    
    - update store service to use the new request manager;

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,36 @@
 import Route from '@ember/routing/route';
 
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
+import { query } from '@ember-data/json-api/request';
 
 export default class IndexRoute extends Route {
+  // old:
+
+  // async model() {
+  //   let response = await fetch('/api/rentals.json');
+
+  //   let { data } = await response.json();
+
+  //   return data.map((model) => {
+  //     let { id, attributes } = model;
+  //     let type;
+
+  //     if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //       type = 'Community';
+  //     } else {
+  //       type = 'Standalone';
+  //     }
+
+  //     return { id, type, ...attributes };
+  //   });
+  // }
+
+  // new:
+
+  @service store;
+
   async model() {
-    let response = await fetch('/api/rentals.json');
-
-    let { data } = await response.json();
-
-    return data.map((model) => {
-      let { id, attributes } = model;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    });
+    const { content } = await this.store.request(query('rental'));
+    return content.data;
   }
 }
